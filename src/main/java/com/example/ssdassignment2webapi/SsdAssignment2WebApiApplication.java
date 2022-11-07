75da6382d08724465a78c7ce6445f61034e33fc1
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-11-07 21:05:38 +0530

    Added MongoConfiguration and disabled auto configuration for Java.

--- src/main/java/com/example/ssdassignment2webapi/SsdAssignment2WebApiApplication.java
+++ src/main/java/com/example/ssdassignment2webapi/SsdAssignment2WebApiApplication.java
@@ -2,8 +2,10 @@ package com.example.ssdassignment2webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class SsdAssignment2WebApiApplication {
 
     public static void main(String[] args) {
